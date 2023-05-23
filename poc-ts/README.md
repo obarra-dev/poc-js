1d6b9c58859ed6a52b09fe5fe313bfc8765fffac
obarra <omarbarra@pop-os.localdomain>
2023-05-23 16:05:28 -0300

    add jest

--- poc-ts/README.md
+++ poc-ts/README.md
@@ -5,3 +5,11 @@ npx tsc --init
 npx tsc
 npx tsc -w
 npx ts-node index.ts 
+
+
+npm install --save-dev --save-exact prettier
+
+npm install --save-dev jest
+npm install --save-dev @types/jest
+
+npm install --save-dev ts-jest
---
+++ poc-ts/src/sum.test.ts
@@ -0,0 +1,18 @@
+test('adds 1 + 2 to equal 3', () => {
+    expect(3).toBe(3);
+  });
+  
+  
+  describe("Potter Store price check", () => {
+      it("should return 8.0 if you only get one copy of the 1st book", () => {
+        expect(8.0).toBe(8.0);
+      });
+    
+  
+      it("should buy all the bugs with repeated versions for 1, 2 and 3", () => {
+        expect(
+          5.0
+        ).toBe(5.0);
+      });
+    });
+    
