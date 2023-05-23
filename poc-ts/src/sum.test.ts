c0083a18d4b4c15c7fad8676971f2e54f81d20bc
obarra <omarbarra@pop-os.localdomain>
2023-05-23 16:30:42 -0300

    clean

--- poc-ts/src/sum.test.ts
+++ poc-ts/src/sum.test.ts
@@ -1,18 +1,13 @@
-test('adds 1 + 2 to equal 3', () => {
-    expect(3).toBe(3);
+test("adds 1 + 2 to equal 3", () => {
+  expect(3).toBe(3);
+});
+
+describe("groped tests", () => {
+  it("test one", () => {
+    expect(8.0).toBe(8.0);
   });
-  
-  
-  describe("Potter Store price check", () => {
-      it("should return 8.0 if you only get one copy of the 1st book", () => {
-        expect(8.0).toBe(8.0);
-      });
-    
-  
-      it("should buy all the bugs with repeated versions for 1, 2 and 3", () => {
-        expect(
-          5.0
-        ).toBe(5.0);
-      });
-    });
-    
+
+  it("test two", () => {
+    expect(5.0).toBe(5.0);
+  });
+});
