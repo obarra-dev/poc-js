957b1ec7716c990594882bf3883851dad84a0a9d
obarra <omarbarra@pop-os.localdomain>
2023-10-13 00:46:28 -0300

    more generic

--- poc-ts/generics.ts
+++ poc-ts/generics.ts
@@ -69,3 +69,48 @@ dataCollectionProduct.addItem(product)
 console.log(dataCollectionProduct.getItems())
 console.log(dataCollectionProduct.getNames())
 console.log(dataCollectionProduct.getItemById(3))
+
+
+interface Resource<T> {
+    uid: number,
+    name: string,
+    data: T,
+}
+
+const r1: Resource<string> = {
+    uid: 1,
+    name: 'string',
+    data: 'data string',
+}
+console.log(r1)
+
+const r2: Resource<object> = {
+    uid: 2,
+    name: 'object',
+    data:  {age: 17},
+}
+console.log(r2)
+
+const r3: Resource<string[]> = {
+    uid: 3,
+    name: 'array',
+    data:  ['one', 'one'],
+}
+console.log(r3)
+
+
+// esto esta raro
+const addUUID = <T>(obj: T) => {
+    const uuid = Math.floor(Math.random() * 100)
+    return {...obj, uuid}
+}
+
+// raro, puede inferir pero no le defini el tipo ni el generic ni en el return
+const some = addUUID({name: 'name', age: 30})
+console.log(some.age)
+
+
+// raro infiere pero, esta mal xq retorna un objet
+const u = addUUID<string>('sdgs')
+console.log(u.length)
+console.log(u.uuid)
